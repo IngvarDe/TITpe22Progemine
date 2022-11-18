302de3088b1acbf9c6ab43c2ba8c2f7007313990
Ingvar Derešivski <inde999@gmail.com>
2022-11-18 13:04:46 +0200

    If ja Else, kus muutuja on ette antud

---
+++ 18.11.2022 IfAndElse/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // muutuja defineerimine, kirjutan programmile, et y on n[[d  t'isarvuga muutuja
+            int y;
+            // siin annan y-le v''rtuse ja selleks on 22
+            y = 22;
+
+            // kontrollin, kas y v]rdub 20 v]i mitte. Kui ei v]rdu, siis kuvab teksti,
+            // mis j''b peale else
+            if (y == 20)
+            {
+                Console.WriteLine("The value is 20 and condition is true");
+            }
+            else
+            {
+                Console.WriteLine("The value is not 20 so conition is false");
+            }
+        }
+    }
+}
