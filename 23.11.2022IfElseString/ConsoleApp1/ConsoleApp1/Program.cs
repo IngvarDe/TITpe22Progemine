9a70612f6cc16a986070b2141b510d7756fd0b33
Ingvar Derešivski <inde999@gmail.com>
2022-11-23 10:48:48 +0200

    IfElseString

---
+++ 23.11.2022IfElseString/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IfElseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mis värvi soovid valida? (inglise keeles)");
+            string color = Console.ReadLine();
+
+
+            if (color == "red")
+            {
+                Console.WriteLine("See on punane värv");
+                Console.ReadLine();
+            }
+            else if (color == "blue")
+            {
+                Console.WriteLine("See on sinine värv");
+                Console.ReadLine();
+            }
+            else if (color == "green")
+            {
+                Console.WriteLine("See on roheline värv");
+                Console.ReadLine();
+            }
+            else if (color == "white")
+            {
+                Console.WriteLine("See on valge värv");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Värvi ei sisestatud või oli vale värvi nimetus");
+            }
+        }
+    }
+}
