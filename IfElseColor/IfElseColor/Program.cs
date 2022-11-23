7dd034a509deea706f485f54f5afc390fef9bb63
Ingvar Derešivski <inde999@gmail.com>
2022-11-23 11:30:01 +0200

    IfElseTextColor

---
+++ IfElseColor/IfElseColor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IfElseColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta nimi:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Tere, " + name + " !");
+
+            Console.WriteLine("Nimi on sisestatud ja nüüd hakkab kontroll");
+
+            if (name != "")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.White;
+                //seda teksti kuvatakse rohelisena
+                Console.WriteLine("Tere, " + name + " !");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                //seda teksti kuvatakse punasena
+                Console.WriteLine("Nime ei sisestatud.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
