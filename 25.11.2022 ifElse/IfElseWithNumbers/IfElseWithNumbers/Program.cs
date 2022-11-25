f8461d699fd6ab3b6cec49439b7b74dcc49983b5
Ingvar Derešivski <inde999@gmail.com>
2022-11-25 12:25:11 +0200

    IfElseNumbrid

---
+++ 25.11.2022 ifElse/IfElseWithNumbers/IfElseWithNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IfElseWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 1;
+            int b = 2;
+
+            if (a == 1)
+            {
+                Console.WriteLine("a on 1");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("a on võrdne b-ga");
+            }
+            else if (a != b)
+            {
+                Console.WriteLine("a ei ole v]rdne b-ga");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
