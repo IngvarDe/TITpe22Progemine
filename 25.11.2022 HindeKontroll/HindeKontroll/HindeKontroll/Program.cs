877b002df2948ca72c2084ec48409ab8ec02b770
Ingvar Derešivski <inde999@gmail.com>
2022-11-25 13:40:27 +0200

    IfElseHindeKontroll

---
+++ 25.11.2022 HindeKontroll/HindeKontroll/HindeKontroll/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HindeKontroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta hinde number: ");
+            // muudab string andmetyybi int andmetüübiks
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0 || num > 100)
+            {
+                Console.WriteLine("Vale number");
+            }
+            else if (num >= 0 && num < 50)
+            {
+                Console.WriteLine("Failer oled ja luuser");
+            }
+            else if (num >= 50 && num < 60)
+            {
+                Console.WriteLine("D hinde saad");
+            }
+            else if (num >= 60 && num < 70)
+            {
+                Console.WriteLine("C hinde saad");
+            }
+            else if (num >= 70 && num < 80)
+            {
+                Console.WriteLine("B hinde saad");
+            }
+            else if (num >= 80 && num < 90)
+            {
+                Console.WriteLine("A hinde saad");
+            }
+            else if (num >= 90 && num < 100)
+            {
+                Console.WriteLine("C hinde saad");
+            }
+            else
+            {
+                Console.WriteLine("Tubli, istu, kaks");
+            }
+        }
+    }
+}
