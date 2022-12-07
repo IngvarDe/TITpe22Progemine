111af5e9ad9d1d9403e820cb6869c1c70aa868b4
Ingvar Derešivski <inde999@gmail.com>
2022-12-07 11:41:33 +0200

    07.12.2022 ForLoop

---
+++ 07.12.2022 For8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta ridade arv:");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            // teha sellest flowchart
+        }
+    }
+}
