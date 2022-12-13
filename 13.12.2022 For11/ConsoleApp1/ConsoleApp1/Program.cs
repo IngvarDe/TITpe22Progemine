10a974fc3229f264dc24bbcd80bf8fffe45decf6
Ingvar Derešivski <inde999@gmail.com>
2022-12-13 08:57:49 +0200

    13.12.2022ForLoop

---
+++ 13.12.2022 For11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //1
+            //22
+            //333
+            //4444
+            //55555
+
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta ridade number");
+            rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(i++);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ 13.12.2022 For12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, spc, rows, k, t = 1;
+
+            Console.WriteLine("Numbri püramiid");
+
+            Console.WriteLine("Sisesta ridade arv");
+
+            rows = Convert.ToInt32(Console.ReadLine());
+            spc = rows + 4 - 1;
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (k = spc; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+        }
+    }
+}
