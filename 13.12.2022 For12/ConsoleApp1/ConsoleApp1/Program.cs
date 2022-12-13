d5de4d89ceaeba17116a2b139b77fdb1d2f3e14a
Ingvar Derešivski <inde999@gmail.com>
2022-12-13 09:49:27 +0200

    13.12.2022ForLoopTeemant

--- 13.12.2022 For12/ConsoleApp1/ConsoleApp1/Program.cs
+++ 13.12.2022 For12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,23 +6,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int i, j, spc, rows, k, t = 1;
+            int i, j, rows;
 
             Console.WriteLine("Numbri püramiid");
 
             Console.WriteLine("Sisesta ridade arv");
 
             rows = Convert.ToInt32(Console.ReadLine());
-            spc = rows + 4 - 1;
+            
 
-            for (i = 1; i <= rows; i++)
+            for (i = 0; i <= rows; i++)
             {
-                for (k = spc; k >= 1; k--)
+                for (j = 1; j <= rows-i; j++)
                 {
                     Console.Write(" ");
                 }
+                for (j = 1; j <= 2*i-1; j++)
+                {
+                    //Console.Write("{0} ", t++);
+                    Console.Write("*"); //kui paned selle, siis saad püramiidi
+                }
+                Console.Write("\n");
             }
 
+            for (i = rows - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*"); 
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
