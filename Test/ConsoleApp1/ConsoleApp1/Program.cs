35ad7222d21be2fe2dc794a9000bd7f6674f10eb
Ingvar Derešivski <inde999@gmail.com>
2022-12-14 09:05:02 +0200

    14.12.2022Shapes

---
+++ Test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareSize;
+
+            Console.WriteLine("palun sisesta ruudu suurus: ");
+            squareSize = Int32.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= squareSize; row++)
+            {
+                for (int column = 1; column <= squareSize; column++)
+                {
+                    string string1;
+
+                    string1 = (row == column || row + column == squareSize + 1) ? " X " : " 0 ";
+                    //täpselt sama, mis alumine if ja else
+
+                    //if (row == column || row + column == squareSize + 1)
+                    //{
+                    //    string1 = " X ";
+                    //}
+                    //else
+                    //{
+                    //    string1 = " 0 ";
+                    //}
+                    Console.Write(string1);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
